f6d3d9582711b3f19f8680c1d69a861db3f568cd
Kirill Anikin <kirill_anikin@list.ru>
2021-02-28 18:25:44 +0700

    add second array

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
 using namespace std;
+#define tab "\t"
 
 int main()
 {
 	
 	setlocale(LC_ALL, "");
 	cout << "Hello Functions!"<< endl;
+	const int n = 5;
+	int arr[n];
+	// fill the array with random numbers
+
+	for (int i=0; i<n; i++)
+	{
+		arr[i] = rand(); // random from 0 to 32 676
+
+	}
+	for (int x : arr) cout << x << tab; cout << endl;
+
+	int number_of_shifts;
+	cout << "Input number of shifts"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n; j++)
+		{
+			arr[j] = arr[j + 1];
+
+		}
+		arr[n - 1] = buffer;
+	}
+	for (int x : arr) cout << x << tab; cout << endl;
+
+
+	const int m = 5;
+	int brr[m];
+	for (int i = 0; i < m; i++)
+	{
+		brr[i] = rand();
+	}
+
+	for (int x : brr) cout << x << tab; cout << endl;
+
+	cout << "Input number of shifts"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = brr[0];
+		for (int j = 0; j < m; j++)
+		{
+			brr[j] = brr[j + 1];
+
+		}
+		brr[m - 1] = buffer;
+	}
+	for (int x : brr) cout << x << tab; cout << endl;
+
 	return 0;
 }
